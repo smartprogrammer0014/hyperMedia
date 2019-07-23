7fab3cb1737eb4f97882ecbab1a72827476ffbcd
Mukesh pandey <30684585+smartprogrammer0014@users.noreply.github.com>
2019-07-24 00:45:47 +0530

    Test object created and m1() called.

--- temp.java
+++ temp.java
@@ -2,6 +2,14 @@ class Test
 {
   public static void main(String[] args)
   {
+    Test t = new Test();
     System.out.println("Nice World");
+    
+    public void m1()
+    {
+      System.out.println("This is m1 method");
+    }
+    
+    t.m1();
   }
 }
